c2ed35ce2b9080a493320edde9527c3249869748
Your Name <you@example.com>
2022-01-24 21:21:10 +0800

    your_message

--- prod_setting.py
+++ prod_setting.py
@@ -1,6 +1,6 @@
-from .setting import *
+from .settings import *
 import dj_database_url
-DATABASE = {
+DATABASES = {
     'default': dj_database_url.config()
 }
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
