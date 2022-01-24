5d46dea23eb59e7f7a507cbe237e3e257eb29bc2
Your Name <you@example.com>
2022-01-24 21:11:35 +0800

    your_message

--- prod_setting.py
+++ prod_setting.py
@@ -1,5 +1,7 @@
 from .setting import *
-STATIC_ROOT = 'staticfiles'
+import dj_database_url
+DATABASE = {
+    'default': dj_database_url.config()
+}
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
-DEBUG = False
